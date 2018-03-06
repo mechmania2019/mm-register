d35e2beadb6711448f26056560e070af6718db83
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-06 10:41:09 -0600

    changed to service

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const { send } = require('micro')
+const mongoose = require('mongoose')
 
-const register = mongoose => {
+mongoose.connect(process.env.MONGO_URL)
+mongoose.Promise = global.Promise
+
+module.exports = async (req, res) => {
   const { Team } = require('mm-schemas')(mongoose)
-  return fn => async(req, res) => {
-    const header = req.headers.authorization
-    if(!header) {
-      return send(res, 401, 'Unauthorized')
-    }
-    // Assumed Format: https://user:password@domain.com
-    const [email, psswd] = header.split(':') 
-    const team = await Team.find({email: email })
-    if (!team){
-      return send(res, 401, 'Team already exists')
-    }
-    const data = await json(req)
-    const newTeam = new Team({
-      name: data.name,
-      admin: data.admin || false,
-      email: email,
-      password: passwd,
-    })
-    res.user = newTeam
-    return await fn(req,res)  
-  }
-}
+  const data = await json(req)
 
-module.exports = register;
+  // check if team exists or not
+  const team = await Team.find({name: data.name })
+  if (team){
+    return send(res, 401, 'Team already exists')
+  }
+  
+  const newTeam = new Team({
+    name: data.name,
+    email: data.email
+  })
+  newTeam.save()
+
+  return newTeam;
+}
