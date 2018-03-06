2bfddc21c8738951b2d89ab2e17848cecd713450
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-05 23:17:21 -0600

    add other stuff

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const { send } = require('micro')
 
-
 const register = mongoose => {
-    const { Team } = require('mm-schemas')(mongoose)
-    return fn => async(req, res) => {
-
+  const { Team } = require('mm-schemas')(mongoose)
+  return fn => async(req, res) => {
+    const header = req.headers.authorization
+    if(!header) {
+      return send(res, 401, 'Unauthorized')
     }
-
-
+    // Assumed Format: https://user:password@domain.com
+    const [email, psswd] = header.split(':') 
+    const team = await Team.find({email: email })
+    if (!team){
+      return send(res, 401, 'Team already exists')
+    }
+    const data = await json(req)
+    const newTeam = new Team({
+      name: data.name,
+      admin: data.admin || false,
+      email: email,
+      password: passwd,
+    })
+    res.user = newTeam
+    return await fn(req,res)  
+  }
 }
 
 module.exports = register;
