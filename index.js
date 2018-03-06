60f63966663fa2ebbaa5bfc5987017a4d6986c22
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-05 22:51:15 -0600

    added mm-schemas

---
+++ index.js
@@ -0,0 +1,13 @@
+const { send } = require('micro')
+
+
+const register = mongoose => {
+    const { Team } = require('mm-schemas')(mongoose)
+    return fn => async(req, res) => {
+
+    }
+
+
+}
+
+module.exports = register;
