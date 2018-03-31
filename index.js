153aef180b4bc0754fb3014635f0dec411e6a4ca
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-31 15:49:02 -0500

    works now

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ const { send, json } = require('micro')
 const mongoose = require('mongoose')
 const { Team } = require('mm-schemas')(mongoose)
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
 mongoose.Promise = global.Promise
 
 module.exports = async (req, res) => {
-  const data = await json(req)
+  try {
+    const data = await json(req)
+    // check if team exists or not
+    const team = await Team.find({ name: data.name })
+    console.log(team)
+    if (team.length !== 0) {
+      return send(res, 401, 'Team already exists')
+    }
 
-  // check if team exists or not
-  const team = await Team.find({ name: data.name })
-  if (team) {
-    return send(res, 401, 'Team already exists')
+    const newTeam = new Team({
+      name: data.name,
+      email: data.email
+    })
+    newTeam.save()
+
+    return newTeam
+  } catch (err) {
+    return send(res, 400, err.message)
   }
-
-  const newTeam = new Team({
-    name: data.name,
-    email: data.email
-  })
-  newTeam.save()
-
-  return newTeam
 }
