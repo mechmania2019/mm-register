5066869d047dc868763969a93d37945ad68975e0
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-31 15:22:27 -0500

    stuff

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const { send } = require('micro')
+const { send, json } = require('micro')
 const mongoose = require('mongoose')
+const { Team } = require('mm-schemas')(mongoose)
 
 mongoose.connect(process.env.MONGO_URL)
 mongoose.Promise = global.Promise
 
 module.exports = async (req, res) => {
-  const { Team } = require('mm-schemas')(mongoose)
   const data = await json(req)
 
   // check if team exists or not
-  const team = await Team.find({name: data.name })
-  if (team){
+  const team = await Team.find({ name: data.name })
+  if (team) {
     return send(res, 401, 'Team already exists')
   }
-  
+
   const newTeam = new Team({
     name: data.name,
     email: data.email
   })
   newTeam.save()
 
-  return newTeam;
+  return newTeam
 }
