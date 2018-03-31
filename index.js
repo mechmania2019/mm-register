a766e2856f6aef3fb60a388322aad34ee70a8465
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-31 17:14:35 -0500

    added cors

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const { send, json } = require('micro')
 const mongoose = require('mongoose')
 const { Team } = require('mm-schemas')(mongoose)
+const cors = require('micro-cors')()
 
 mongoose.connect(process.env.MONGO_URI)
 mongoose.Promise = global.Promise
 
-module.exports = async (req, res) => {
+const handler = async (req, res) => {
   try {
     const data = await json(req)
+    console.log('Recieved Request: ', data)
     // check if team exists or not
     const team = await Team.find({ name: data.name })
-    console.log(team)
     if (team.length !== 0) {
       return send(res, 401, 'Team already exists')
     }
@@ -26,3 +27,4 @@ module.exports = async (req, res) => {
     return send(res, 401, err.message)
   }
 }
+module.exports = cors(handler)
